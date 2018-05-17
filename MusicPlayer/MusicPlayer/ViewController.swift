21bc34bdb3f65fff6e0dbf8410dadd166dc5a1ad
shindk8659 <shindk8659@naver.com>
2018-05-18 01:09:18 +0900

    start making Musicplayer project - add image

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 신동규 on 2018. 5. 18..
+//  Copyright © 2018년 신동규. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
